e19ecb4e3ac10495bb298dbfd983dcde53c22775
Advaith Krupanandh <advaith.krupanandh@gmail.com>
2024-05-30 02:23:41 +0530

    Indentation & deleted whitespace in css
    Also aligned all code to 4 space indentation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import SearchApp from './SearchApp/SearchApp';
+import "./App.css";
+import SearchApp from "./SearchApp/SearchApp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ja<span>mmm</span>ing</h1>
-        <h3>Search for music, find your jam!</h3>
-      </header>
-      <body>
-        <SearchApp />
-      </body>
-      <footer>This project uses the Spotify Web API</footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    Ja<span>mmm</span>ing
+                </h1>
+                <h3>Search for music, find your jam!</h3>
+            </header>
+            <body>
+                <SearchApp />
+            </body>
+            <footer>This project uses the Spotify Web API</footer>
+        </div>
+    );
 }
 
 export default App;
