fc1339b29b805d24a14f076315d1524ef6e2849b
Advaith Krupanandh <advaith.krupanandh@gmail.com>
2024-06-05 02:54:30 +0530

    Implemented eveyrthing except `savePlaylistToSpotify()`
    1. Fixed `searchSpotify()` & `authorizeSpotify()`. Removed hardcoded access token. Now only using client ID and client secret to generate the token.
    2. In SearchArea, added code to execute `handleSearch()` if enter button is pressed.
    3. Implemented the `isValidate()` function inside the `ResultAndPlaylistArea` component.
    4. Implemented a loading component incase search API is being called.
    5. Changed alignment of artist/album inside the `SongItem` component.
    6. Added "Save to Spotify" button with CSS.

--- src/SearchApp/SearchApp.js
+++ src/SearchApp/SearchApp.js
@@ -7,68 +7,78 @@ const SearchApp = () => {
     const [searchResults, setSearchResults] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    // Function to call Spotify APIs and get search results
-    const searchSpotify = async () => {
-        try {
-            setLoading(true);
+    /* Spotify API Integration */
+        let spotifyToken = null;
+        let tokenExpirationTime = null;
 
-            // const token = await authorizeSpotify();
-
-            const myHeaders = new Headers();
-            myHeaders.append("Authorization", "BQD4pbPWyKZ3isspbgsKMVxQLfFatb_sAlTgW7O21yRCZbbHPxj9csSqygpU3mODV98zidpT1fqCaMGA2t9_P5zsla6QTfmkaeESjbRsSzEu1znagFE");
-
-            const requestOptions = {
-                method: "GET",
-                headers: myHeaders,
-                redirect: "follow"
-            };
-
-            const response = await fetch(`https://api.spotify.com/v1/search?q=${searchText}&type=track&limit=10`, requestOptions)
-                .then((response) => response.text())
-                .then((result) => console.log(result))
-                .catch((error) => console.error(error));
-            
-            const data = await response.json();
-            const tracks = data.tracks.items.map((track) => {
-                return {
-                    id: track.id,
-                    name: track.name,
-                    artist: track.artists[0].name,
-                    album: track.album.name,
+        // Search Spotify API - To search for inputted text
+        const searchSpotify = async () => {
+            try {
+                setLoading(true);
+                const token = await authorizeSpotify();
+                const myHeaders = new Headers();
+                myHeaders.append("Authorization", `Bearer ${token}`);
+                const requestOptions = {
+                    method: "GET",
+                    headers: myHeaders,
+                    redirect: "follow"
                 };
-            })
-            setSearchResults([...tracks]);
-        } catch (error) {
-            console.error('Error fetching search results: ', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const authorizeSpotify = async () => {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-        myHeaders.append("Cookie", "__Host-device_id=AQCKwWTKDJHdvYP69rdFkA-7E8OlDi234x7ZZs8wfyuOBRY5EK9qVUOwfHs-VC6RzKhzXSLFSTGy8duzfQUdZSZievCjornL7-g; sp_tr=false");
-
-        const urlencoded = new URLSearchParams();
-        urlencoded.append("grant_type", "client_credentials");
-        urlencoded.append("client_id", "624be565b6964802ad1354c478898c64");
-        urlencoded.append("client_secret", "bf2799a091514410a5de7ce04fdb5008");
-
-        const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: urlencoded,
-            redirect: "follow"
+                const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(searchText)}&type=track&limit=10`, requestOptions);
+                if (!response.ok) {
+                    console.error('HTTP error: ', response.status);
+                    const errorText = await response.text();
+                    console.error('Error response: ', errorText);
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                const tracks = data.tracks.items.map((track) => {
+                    return {
+                        id: track.id,
+                        name: track.name,
+                        artist: track.artists[0].name,
+                        album: track.album.name,
+                    };
+                });
+                setSearchResults([...tracks]);
+            } catch (error) {
+                console.error('Error fetching search results: ', error);
+            } finally {
+                setLoading(false);
+            }
         };
 
-        const token = await fetch("https://accounts.spotify.com/api/token", requestOptions)
-            .then((response) => {return response.text()})
-            .then((result) => console.log(result))
-            .catch((error) => console.error(error));
+        // Get Access token if previous one is expired
+        const authorizeSpotify = async () => {
+            const currentTime = Date.now();
+            if (spotifyToken && tokenExpirationTime && currentTime < tokenExpirationTime) {
+                return spotifyToken;
+            }
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+            const urlencoded = new URLSearchParams();
+            urlencoded.append("grant_type", "client_credentials");
+            urlencoded.append("client_id", "624be565b6964802ad1354c478898c64");
+            urlencoded.append("client_secret", "bf2799a091514410a5de7ce04fdb5008");
+            const requestOptions = {
+                method: "POST",
+                headers: myHeaders,
+                body: urlencoded,
+                redirect: "follow"
+            };
+            const response = await fetch("https://accounts.spotify.com/api/token", requestOptions);
+            if (!response.ok) {
+                console.error('HTTP error: ', response.status);
+                const errorText = await response.text();
+                console.error('Error response: ', errorText);
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            spotifyToken = data.access_token;
+            tokenExpirationTime = currentTime + (data.expires_in * 1000);
+            return spotifyToken;
+        };
+    /* Spotify API Integration */
 
-        return token;
-    }
 
     return (
         <div>
@@ -77,7 +87,7 @@ const SearchApp = () => {
                 setSearchText={setSearchText}
                 handleSearch={searchSpotify}
             />
-            <ResultAndPlaylistArea 
+            <ResultAndPlaylistArea
                 searchResults={searchResults}
                 setSearchResults={setSearchResults}
                 loading={loading}
@@ -94,6 +104,11 @@ const SearchArea = ({searchText, setSearchText, handleSearch}) => {
                 placeholder="Search for any song"
                 value={searchText}
                 onChange={(e) => {setSearchText((e.target.value))}}
+                onKeyDown={(e) => {
+                    if(e.key === 'Enter') {
+                        handleSearch();
+                    }
+                }}
             />
             <button type="button" onClick={handleSearch}>
                 Search on <b>Spotify</b>
@@ -104,7 +119,29 @@ const SearchArea = ({searchText, setSearchText, handleSearch}) => {
 
 const ResultAndPlaylistArea = ({searchResults, setSearchResults, loading}) => {
 
+    const [playlistName, setPlaylistName] = useState('');
     const [playlist, setPlaylist] = useState([]);
+
+    // Save playlist to Spotify
+    const savePlaylistToSpotify = async () => {
+        if(isValidate()) {
+
+        }
+    }
+
+    // To validate submit values
+    const isValidate = () => {
+        let isValid = true;
+        if (playlistName.trim() === '') {
+            alert('Please enter a playlist name');
+            isValid = false;
+        }
+        if (playlist.length === 0) {
+            alert('Please add at least one song to the playlist');
+            isValid = false;
+        }
+        return isValid;
+    }
 
     const handleAdd = (songData) => {
         setPlaylist([...playlist, songData]);
@@ -116,6 +153,7 @@ const ResultAndPlaylistArea = ({searchResults, setSearchResults, loading}) => {
         setSearchResults([songData, ...searchResults]);
     }
 
+
     return (
         <div className={"result_playlist_area"}>
             <SearchResultsArea 
@@ -126,6 +164,9 @@ const ResultAndPlaylistArea = ({searchResults, setSearchResults, loading}) => {
             <PlaylistArea 
                 playlist={playlist}
                 handleRemove={handleRemove}
+                playlistName={playlistName}
+                setPlaylistName={setPlaylistName}
+                savePlaylistToSpotify={savePlaylistToSpotify}
             />
         </div>
     )
@@ -133,18 +174,45 @@ const ResultAndPlaylistArea = ({searchResults, setSearchResults, loading}) => {
 
 const SearchResultsArea = ({searchResults, loading, handleAdd}) => {
     return (
-        <div className={"search_results"}>
+        <div className="search_results">
             <h2>Search Results</h2>
             <hr />
-            {searchResults.map((songData) => <SongItem key={`searchresults_${songData.id}`} songData={songData} type="add" handleAdd={handleAdd} />)}
+            {loading ? 
+                (<Loading />)
+                : 
+                (searchResults.map((songData) => (
+                    <SongItem
+                        key={`searchresults_${songData.id}`}
+                        songData={songData}
+                        type="add"
+                        handleAdd={handleAdd}
+                    />
+                )))
+            }
         </div>
-    )
+    );
 }
 
-const PlaylistArea = ({playlist, handleRemove}) => {
+const Loading = () => {
+    return <div className="loading-spinner"></div>;
+};
+
+const PlaylistArea = ({playlist, handleRemove, playlistName, setPlaylistName, savePlaylistToSpotify}) => {
     return (
         <div className={"playlist_area"}>
-            {playlist.map((songData) => <SongItem key={`playlist_${songData.id}`} songData={songData} type="remove" handleRemove={handleRemove} />)}
+            <input 
+                type="text"
+                placeholder="Playlist Name" 
+                value={playlistName} 
+                onChange={(e) => setPlaylistName(e.target.value)}>
+            </input>
+            <hr />
+            <div className={"playlist_tracks"}>
+                {playlist?.map((songData) => <SongItem key={`playlist_${songData.id}`} songData={songData} type="remove" handleRemove={handleRemove} />)}
+            </div>
+            <button type="button" onClick={() => {savePlaylistToSpotify()}}>
+                Save to Spotify
+            </button>
         </div>
     )
 }
@@ -155,7 +223,8 @@ const SongItem = ({songData, type, handleAdd, handleRemove}) => {
             <div className={"song_data"}>
                 <h3>{songData.name}</h3>
                 <hr />
-                <div className={"song_artist"}>{songData.artist} | {songData.album}</div>
+                <div className={"song_details"}>Artist: <span>{songData.artist}</span></div>
+                <div className={"song_details"}>Album: <span>{songData.album}</span></div>
             </div>
             <button className={"song_button"} type="button" onClick={() => type === "add" ? handleAdd(songData) : handleRemove(songData)}>
                 {type === "add" ? "+" : "-"}
